67619b9759a08621323c715982f808ab275c40d5
Alireza Fatemi <alirezafa@rocketmail.com>
2020-07-25 22:04:41 +0430

    Improved grammar.

--- lark-decaf-compiler/src/parser.py
+++ lark-decaf-compiler/src/parser.py
@@ -62,31 +62,44 @@ break_stmt: "break" ";"
 
 print_stmt : "Print" "(" expr ("," expr)* ")" ";" -> print
 
-expr : l_value "=" expr
+expr : assignment
     | constant
     | l_value
     | "this"
     | call
     | "(" expr ")"
-    | expr "+" expr
-    | expr "-" expr
-    | expr "*" expr
-    | expr "/" expr
-    | expr "%" expr
-    | "-" expr
-    | expr "<=" expr
-    | expr "<" expr
-    | expr ">=" expr
-    | expr ">" expr
-    | expr "==" expr
-    | expr "!=" expr
-    | expr "&&" expr
-    | expr "||" expr
-    | "!" expr
+    | minus
+    | not
+    | multiplication
+    | division
+    | modulo
+    | addition
+    | subtraction
+    | inequality
+    | equality
+    | logical_and
+    | logical_or
     | "ReadInteger" "(" ")" -> read_integer
     | "ReadLine" "(" ")"
     | "new" IDENT
     | "NewArray" "(" expr "," type ")"
+
+minus.8: "-" expr -> minus
+not.8: "!" expr -> not 
+multiplication.7: expr "*" expr -> multiplication
+division.7: expr "/" expr -> division
+modulo.7: expr "%" expr -> modulo
+addition.6: expr "+" expr -> addition 
+subtraction.6: expr "-" expr -> subtraction
+inequality.5: expr "<=" expr -> inequality 
+    | expr "<" expr -> inequality
+    | expr ">=" expr -> inequality
+    | expr ">" expr -> inequality
+equality.5: expr "==" expr -> equality
+    | expr "!=" expr -> equality
+logical_and.4: expr "&&" expr -> logical_and 
+logical_or.4: expr "||" expr -> logical_or
+assignment: l_value "=" expr -> assignment
 
 l_value: IDENT
     | expr "." IDENT
